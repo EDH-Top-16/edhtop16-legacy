63a16bb57d645bc0faac15d49a810c1352811ca0
Jason Qiu <jqiu21@protonmail.com>
2023-09-01 10:09:13 -0400

    Add global rate limit to prevent ddos

--- server/middleware/rate_limit.js
+++ server/middleware/rate_limit.js
@@ -9,4 +9,14 @@ const rateLimiter = rateLimit({
   legacyHeaders: false,
 });
 
-module.exports = { rateLimiter };
+const globablRateLimit = rateLimit({
+  windowMS: 60 * 1000, // 1 min
+  max: 12,
+  message: "Too many requests. Please try again in 1 min.",
+  statusCode: 429,
+  standardHeaders: true,
+  legacyHeaders: false,
+  keyGenerator: () => 'global'
+})
+
+module.exports = { rateLimiter, globablRateLimit };
